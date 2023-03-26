7e0a5cfe4a1d64486bd7dbfd6491e2ebaeac6275
jcfernan999 <jcfernan99@gmail.com>
2023-03-26 02:25:58 -0300

    primera commit

---
+++ src/main/java/com/porfolio/fjc/FjcApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.fjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FjcApplication.class, args);
+	}
+
+}
